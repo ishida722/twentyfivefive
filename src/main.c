bc4e06938c2c5da68ae7f26289ab2df42c2c5103
Tadashi_air <lhomme.blanc@me.com>
2016-02-01 23:28:42 +0900

    make window

--- src/main.c
+++ src/main.c
@@ -1,9 +1,33 @@
 #include <pebble.h>
 
+const bool animated = true;
+static Window *standby_window;
+
+
+
+static void standby_window_load(Window *window)
+{
+}
+
+static void standby_window_unload(Window *window)
+{
+}
+
+/* WindowHandlers standby_window_handler */
+
+
+
 static void init(void) {
+	standby_window = window_create();
+	window_set_window_handlers(standby_window, (WindowHandlers) {
+			.load = standby_window_load,
+			.unload = standby_window_unload,
+			});
+	window_stack_push(standby_window, animated);
 }
 
 static void deinit(void) {
+	window_destroy(standby_window);
 }
 
 int main(void) {
