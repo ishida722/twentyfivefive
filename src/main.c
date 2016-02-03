b2587c8460c67701139551cbcb417311df9e499b
Tadashi_air <lhomme.blanc@me.com>
2016-02-03 10:11:26 +0900

    add

--- src/main.c
+++ src/main.c
@@ -76,7 +76,7 @@ static void init(void) {
 	window_set_click_config_provider(standby_window, click_config_provider);
 	window_set_click_config_provider(work_window, click_config_provider);
 	window_set_click_config_provider(rest_window, click_config_provider);
-	window_stack_push(standby_window);
+	window_stack_push(standby_window, animated);
 }
 
 static void deinit(void) {
